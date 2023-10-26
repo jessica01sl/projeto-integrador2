75f85f8f6cc3dcb426e104d34cbdce17bb14da19
philype jorge <lellis.sena@aluno.ifsp.edu.br>
2023-10-26 17:29:47 -0300

    alterações de node e gitgnore

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require("dotenv")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+dotenv.config({path:"./.env"})
 
 var app = express();
 
@@ -18,3 +20,4 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
