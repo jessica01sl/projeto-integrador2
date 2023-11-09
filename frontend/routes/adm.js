17b3ecbcfd93900f95c2ec7a43c7dae060581780
philype <lellis.sena@aluno.ifsp.edu.br>
2023-11-09 09:56:20 -0300

    comit com todas as rotas frontend feitas e toda adm em ejs

--- frontend/routes/adm.js
+++ frontend/routes/adm.js
@@ -2,8 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 
+
+
+
+
+// abaixo rota adm cadastro de adm index pagina inicial da adm
 router.get('/', function(req, res, next) {
-  res.render('pages/adm/index', { title: 'Painel do Administrador', layout: 'layouts/layout' });
+  res.render('pages/adm/index', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
+// rota de motorista da pagina de adm
+router.get('/motorista', function(req, res, next) {
+  res.render('pages/adm/motoristas', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
+
+// rota de motorista da pagina de cadastro 
+router.get('/motorista/cadastrar', function(req, res, next) {
+  res.render('pages/adm/cadastromotorista', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
+
+
+//rota de onibus 
+router.get('/onibus', function(req, res, next) {
+  res.render('pages/adm/motoristas', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
+
+// rota de linhas 
+router.get('/linhas', function(req, res, next) {
+  res.render('pages/adm/motoristas', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
+
+//rota de login da adm 
+router.get('/login', function(req, res, next) {
+  res.render('pages/adm/login', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
+
+
+//rota de cadastro da adm 
+router.get('/cadastrar', function(req, res, next) {
+  res.render('pages/adm/cadastrar', { title: 'Painel do Administrador', layout: 'layout/layout' });
 });
 
 module.exports = router;
