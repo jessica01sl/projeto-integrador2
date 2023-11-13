50018eef5c1bcebcc167bd1dbe4403e99281e702
philype <lellis.sena@aluno.ifsp.edu.br>
2023-11-13 19:22:38 -0300

    coloquei as rotas corretamente para finalização do projeto

--- frontend/routes/adm.js
+++ frontend/routes/adm.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-
-
-
-
 // abaixo rota adm cadastro de adm index pagina inicial da adm
 router.get('/', function(req, res, next) {
   res.render('pages/adm/index', { title: 'Painel do Administrador', layout: 'layout/layout' });
@@ -14,8 +10,8 @@ router.get('/', function(req, res, next) {
 
 
 
-
-// abaixo esta toda a parte de 
+// aqui voce cria uma rota cria um organização la em cima só da parte adm , por exemplo /motorista é onde fica a base dos motoristas e abaixo do barra temos o caminho para o arquivo e falarndo que vai usar o layout padrao e renderizar o que tiver no caminho passadp
+// abaixo esta toda a parte de motorista
 
 
 
@@ -38,6 +34,10 @@ router.get('/motorista/cadastrar', function(req, res, next) {
 
 
 
+//rota de cliente
+router.get('/cliente', function(req, res, next) {
+  res.render('pages/adm/cliente', { title: 'Painel do Administrador', layout: 'layout/layout' });
+});
 
 
 
@@ -45,12 +45,12 @@ router.get('/motorista/cadastrar', function(req, res, next) {
 
 //rota de onibus 
 router.get('/onibus', function(req, res, next) {
-  res.render('pages/adm/motoristas', { title: 'Painel do Administrador', layout: 'layout/layout' });
+  res.render('pages/adm/onibus', { title: 'Painel do Administrador', layout: 'layout/layout' });
 });
 
 // rota de linhas 
 router.get('/linhas', function(req, res, next) {
-  res.render('pages/adm/motoristas', { title: 'Painel do Administrador', layout: 'layout/layout' });
+  res.render('pages/adm/linhas', { title: 'Painel do Administrador', layout: 'layout/layout' });
 });
 
 //rota de login da adm 
