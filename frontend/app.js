8a7280f2b5c1b8891d767087efb48b9c5c966793
philype jorge <lellis.sena@aluno.ifsp.edu.br>
2023-11-14 15:50:55 -0300

    comit com exibir  axeios declarado

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,9 +13,12 @@ var admRouter = require('./routes/adm');
 
 var app = express();
 
+app.set("layout extractScripts", true);
 // view engine setup
+app.set("layout", "layout/layout"); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
