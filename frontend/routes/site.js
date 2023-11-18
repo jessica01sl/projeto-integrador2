196d30d504477ba4be06965f584ef7b38ac6419d
philype <lellis.sena@aluno.ifsp.edu.br>
2023-11-17 22:05:11 -0300

    alterações

--- frontend/routes/site.js
+++ frontend/routes/site.js
@@ -9,46 +9,51 @@ router.get('/home', function (req, res, next) {
 });
 
 // recarga DO SITE PUBLICO
-router.get('/home/recarga', function (req, res, next) {
-  res.render('pages/site/recarga', { title: 'site publico', layout: 'layout/' });
+router.get('/recarga', function (req, res, next) {
+  res.render('pages/site/recarga', { title: 'site publico', layout: 'layout/vazio' });
 });
 
 // horarios DO SITE PUBLICO
-router.get('/home/horarios', function (req, res, next) {
+router.get('/horarios', function (req, res, next) {
   res.render('pages/site/horarios', { title: 'site publico', layout: 'layout/clean' });
 });
 
-// entrar DO SITE PUBLICO
-router.get('/home/login', function (req, res, next) {
-  res.render('pages/site/login', { title: 'site publico', layout: 'layout/clean' });
-});
 
-// cadastrar DO SITE PUBLICO
-router.get('/home/cadastrar', function (req, res, next) {
-  res.render('pages/site/cadastrar', { title: 'site publico', layout: 'layout/clean' });
-});
 
 // criar cartão DO SITE PUBLICO
-router.get('/home/cartao', function (req, res, next) {
+router.get('/cartao', function (req, res, next) {
   res.render('pages/site/criarcartao', { title: 'site publico', layout: 'layout/clean' });
 });
 
 // ajuda cartão DO SITE PUBLICO
-router.get('/home/ajuda', function (req, res, next) {
+router.get('/ajuda', function (req, res, next) {
   res.render('pages/site/ajuda', { title: 'site publico', layout: 'layout/clean' });
 });
 
 // termos de uso  DO SITE PUBLICO
-router.get('/home/termos', function (req, res, next) {
+router.get('/termos', function (req, res, next) {
   res.render('pages/site/termos', { title: 'site publico', layout: 'layout/clean' });
+});
+
+
+// perfil de uso DO SITE PUBLICO
+router.get('/perfil', function (req, res, next) {
+  res.render('pages/site/perfil', { title: 'site publico', layout: 'layout/clean' });
 });
 
 
 
 
+// LOGIN Cde uso DO SITE PUBLICO
+router.get('/login', function (req, res, next) {
+  res.render('pages/site/login', { title: 'site publico', layout: 'layout/vazio' });
+});
 
 
-
+// CADASTRAR de uso DO SITE PUBLICO
+router.get('/cadastrar', function (req, res, next) {
+  res.render('pages/site/cadastrar', { title: 'site publico', layout: 'layout/vazio' });
+});
 
 
 module.exports = router;
