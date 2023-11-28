5c4603ae29bf945b8cbc17ec6888ab1d67ff95a8
philype <lellis.sena@aluno.ifsp.edu.br>
2023-11-28 19:51:09 -0300

    rotas da catraca, paginas ejs da catraca 10%

---
+++ backend/routes/catraca.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- frontend/routes/catraca.js
+++ frontend/routes/catraca.js
@@ -8,8 +8,21 @@ var router = express.Router();
 
 //rota da CATRACA COLOCAR NO LUGAR CORERETO
 router.get('/', function(req, res, next) {
-    res.render('pages/catraca/tela', { title: 'Painel do Administrador', layout: 'layout/vazio' });
+    res.render('pages/catraca/tela', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
   });
+  router.get('/index', function(req, res, next) {
+    res.render('pages/catraca/index', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
+  });
+  router.get('/', function(req, res, next) {
+    res.render('pages/catraca/negado', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
+  });
+  router.get('/liberado', function(req, res, next) {
+    res.render('pages/catraca/liberado', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
+  });
+  router.get('/semsaldo', function(req, res, next) {
+    res.render('pages/catraca/semsaldo', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
+  });
+
   
 
 
