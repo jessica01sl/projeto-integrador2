132f2a2dbc300525ad76e258b3b5988696459851
philype <lellis.sena@aluno.ifsp.edu.br>
2023-11-28 20:43:32 -0300

    ejs completo da catraca faltando alguns scripts

--- frontend/routes/catraca.js
+++ frontend/routes/catraca.js
@@ -7,14 +7,10 @@ var router = express.Router();
 
 
 //rota da CATRACA COLOCAR NO LUGAR CORERETO
+
+// aproximar cartão, liberar, sem saldo, erro desconhecido
 router.get('/', function(req, res, next) {
     res.render('pages/catraca/tela', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
-  });
-  router.get('/index', function(req, res, next) {
-    res.render('pages/catraca/index', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
-  });
-  router.get('/', function(req, res, next) {
-    res.render('pages/catraca/negado', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
   });
   router.get('/liberado', function(req, res, next) {
     res.render('pages/catraca/liberado', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
@@ -22,8 +18,15 @@ router.get('/', function(req, res, next) {
   router.get('/semsaldo', function(req, res, next) {
     res.render('pages/catraca/semsaldo', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
   });
+  router.get('/error', function(req, res, next) {
+    res.render('pages/catraca/error', { title: 'Painel do Administrador', layout: 'layout/catracalayout' });
+  });
 
-  
+
+  // rota de teste do joao apagar
+  router.get('/index', function(req, res, next) {
+    res.render('pages/catraca/index', { title: 'Painel do Administrador', layout: 'layout/vazio' });
+  });
 
 
 
