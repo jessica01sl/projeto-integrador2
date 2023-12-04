ae9c56fc767071fa792a8ff70b504e581b28052c
Philype <lellis.sena@aluno.ifsp.edu.br>
2023-12-04 16:51:29 -0300

    comit com todos os requisitos minimos feijao com arroz pronto completo

--- backend/routes/catraca.js
+++ backend/routes/catraca.js
@@ -8,25 +8,6 @@ const prisma = new PrismaClient();
 
 
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
--- backend/routes/cliente.js
+++ backend/routes/cliente.js
@@ -10,15 +10,15 @@ const prisma = new PrismaClient();
 
 // lisatar cliente--------------------------
 router.get('/listar', async (req, res) => {
-    try {
-      const clientes = await prisma.cliente.findMany();
-      res.json(clientes);
-    } catch (error) {
-      console.error('Erro ao obter clientes:', error);
-      res.status(500).json({ error: 'Erro interno do servidor' });
-    }
-  });
- 
+  try {
+    const clientes = await prisma.cliente.findMany();
+    res.json(clientes);
+  } catch (error) {
+    console.error('Erro ao obter clientes:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
+
 // no gpt: envia o codigo do schema prisma, "express prisma rota/endpoint para ..."
 // quando gerar o codigo trocar de app.js para router.get
 
@@ -80,7 +80,7 @@ router.patch('/recarregar', async (req, res) => {
     const cliente = await prisma.cliente.findUnique({
       where: { id: Number(id) },
     });
-    
+
     console.log(req.body);
 
     if (!cliente) {
@@ -120,7 +120,7 @@ router.patch('/atualizar/:id', async (req, res) => {
   try {
     const clienteId = parseInt(req.params.id);
     const { nome, saldoatual, cpf } = req.body;
-    
+
     // Verifique se o cliente existe antes de tentar atualizá-lo
     const clienteExistente = await prisma.cliente.findUnique({
       where: { id: clienteId },
@@ -265,7 +265,7 @@ router.patch('/tirarsaldo', async (req, res) => {
     const cliente = await prisma.cliente.findUnique({
       where: { id: Number(id) },
     });
-    
+
     console.log(req.body);
 
     if (!cliente) {
@@ -299,6 +299,83 @@ router.patch('/tirarsaldo', async (req, res) => {
 });
 
 
+
+
+
+
+router.post('/liberar', async (req, res, next) => {
+  const clienteId = Number(req.body.clienteId);
+
+  console.log(clienteId);
+  
+  try {
+    // Use uma transação Prisma para garantir atomicidade
+    const novoEmbarque = await prisma.$transaction(async (prisma) => {
+      // Consulte a tabela cliente para obter informações sobre a isenção e o saldo
+      const cliente = await prisma.cliente.findUnique({
+        where: { id: clienteId },
+      });
+
+      console.log(cliente);
+
+      // Verifique se o cliente existe
+      if (!cliente) {
+        throw new Error('Cliente não encontrado');
+      }
+
+      let valorDaPassagem = 5;  // Valor padrão da passagem
+      if (cliente.cartao === "1") { // comum
+        valorDaPassagem = 5;
+      }
+      else if (cliente.cartao === "2") { // estudante
+        valorDaPassagem = 2.5;
+      } else { // idoso=3, pcd=4
+        valorDaPassagem = 0;
+      }
+
+      const isento = cliente.cartao === "3" || cliente.cartao === "4";
+
+      // Verifique se o cliente tem saldo suficiente
+      if (!isento && cliente.saldo < valorDaPassagem) {
+        throw new Error('Saldo insuficiente');
+      }
+
+      // Execute a inserção no banco de dados usando Prisma
+      const embarque = await prisma.embarque.create({
+        data: {
+          clienteId: clienteId,
+          tarifa: valorDaPassagem
+        },
+      });
+
+      // Atualize o saldo do cliente se necessário usando decrement
+      if (!isento) {
+        await prisma.cliente.update({
+          where: { id: clienteId },
+          data: {
+            saldo: {
+              decrement: valorDaPassagem,
+            },
+          },
+        });
+      }
+
+      return embarque;
+    });
+
+    // Envie uma resposta de sucesso
+    res.status(201).json({ message: 'Embarque cadastrado com sucesso!', embarque: novoEmbarque });
+  } catch (error) {
+    console.error('Erro ao cadastrar embarque:', error);
+    if (error.message === 'Cliente não encontrado') {
+      res.status(404).json({ error: 'Cliente não encontrado' });
+    } else if (error.message === 'Saldo insuficiente') {
+      res.status(402).json({ error: 'Saldo insuficiente' }); // 402 Payment Required
+    } else {
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
+});
 
 
 
